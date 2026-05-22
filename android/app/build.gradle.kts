7c49fd3025523e53933fd7ac1df9e18f4a7a006c
Pritesh <im.priteshchitrode@gmail.com>
2026-05-22 17:15:39 +0530

    Fix Bugs

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,8 +24,10 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
     }
 
     defaultConfig {
